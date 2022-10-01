7849c65f7d1e030e3f008670a146d9354ec9c270
RenGVZ <gaving@live.com>
2022-10-01 20:43:23 +0900

    repo setup

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { Welcome, Navbar, Services, Loader, Transactions, Footer } from './components/index'
+const App = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
